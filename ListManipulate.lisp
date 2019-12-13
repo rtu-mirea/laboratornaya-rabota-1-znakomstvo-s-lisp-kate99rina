20caf1a51d238b03ccfa10baf429716237b72d5b
kate99rina <47892341+kate99rina@users.noreply.github.com>
2019-12-13 15:06:28 +0300

    лабораторная работа 1

---
+++ ListManipulate.lisp
@@ -0,0 +1,4 @@
+(nth 2 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15)));; 3 элемент списка
+(nth 1 (nth 4 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; 2 элемент 5 списка
+(+ (nth 1 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))) (nth 3 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; Сумма 2 и 4
+(nth 2 (nth 0 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; 3 элемент 1 списка
